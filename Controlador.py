e1419a249a21db020a6f252136ff30361e9f3aa4
PKS-04 <daniel.oquendo@udea.edu.co>
2025-02-14 13:37:53 -0500

    Add files via upload

--- Controlador.py
+++ Controlador.py
@@ -1,2 +1,43 @@
 from Modelo import EDF,DICOM,Imagen
 from Vista import Ppal
+from PyQt5.QtWidgets import QApplication
+import sys
+
+class Controlador(object):
+    def __init__ (self,vista,edf,dicom):
+        self.__mi_vista=vista
+        self.__mi_edf=edf
+        self.__mi_dicom=dicom
+    def lectoredf(self,edf):
+        return self.__mi_edf.lector_mostraredf(edf)
+    def convertedf(self, edf,dicom):
+        return self.__mi_edf.convertedf(edf,dicom) 
+    
+    def leerdicom(self,dicom):
+        return self.__mi_dicom.leerdicom(dicom)
+    def validardicom(self,dicom):
+        return self.__mi_edf.validardicom(dicom)
+
+    def cargardicom (self,dicom):
+        return self.__mi_dicom.cargardicom(dicom)
+    def convertdicom(self, dicom):
+        return self.__mi_dicom.convertdicom(dicom)
+        
+    def verImagen(self):
+        return self.__mi_dicom.verNifti()
+    
+class Principal(object):
+    def __init__(self): 
+        self.__app = QApplication(sys.argv)
+        self.__mi_vista = Ppal()
+        self.__mi_edf = EDF()
+        self.__mi_dicom = DICOM()
+        self.__mi_controlador = Controlador(self.__mi_vista, self.__mi_edf, self.__mi_dicom)
+        self.__mi_vista.asignarControlador(self.__mi_controlador)
+
+    def main(self):
+        self.__mi_vista.show()
+        sys.exit(self.__app.exec_())  
+
+p=Principal()
+p.main()
--- Vista.py
+++ Vista.py
@@ -12,6 +12,73 @@ from matplotlib.figure import Figure
 class Ppal(QMainWindow):
     def __init__(self):
         super(Ppal, self).__init__()
-        loadUi("Ppal.ui",self)
+        loadUi("InfoII\Ppal.ui",self)
         self.setup()
 
+    def setup(self):
+        self.boton_iniciar.clicked.connect(self.ingresaredf)
+        self.boton_salir.clicked.connect(lambda:self.close())
+
+    def ingresaredf(self):
+        ventana_ruta = Rutaedf(self)
+        self.hide()
+        ventana_ruta.asignarControlador(self.__mi_controlador)
+        ventana_ruta.show()
+        
+    def asignarControlador(self, c):
+        self.__mi_controlador = c
+
+class Rutaedf(QDialog):
+    def __init__(self,parent=None):
+        super().__init__(parent)
+        loadUi("InfoII\Rutaedf.ui",self)
+        self.__ventanapadre=parent
+        self.dicom = None
+        self.setup()
+        
+    def setup (self):
+        self.Ingresar.clicked.connect(self.rutaedf)
+        self.Volver.clicked.connect(self.devolver)
+    def rutaedf(self):
+        self.edf = self.edf.text()
+        self.mostraredf= self.__mi_controlador.lectoredf(self.edf)
+        self.convertedf = self.__mi_controlador.convertedf(self.edf,self.dicom)
+        ventana_ver = MostrarNifti(self)
+        self.hide()
+        ventana_ver.asignarControlador(self.__mi_controlador)
+        ventana_ver.show()
+    def devolver(self):
+        self.__ventanapadre.show()
+        self.hide()
+
+    def asignarControlador(self, c):
+        self.__mi_controlador = c
+
+
+class MostrarNifti(QDialog):
+    def __init__(self, parent = None):
+        super().__init__(parent)
+        self.__ventanapadre=parent
+        self.setup
+    def setup (self):
+        self.Mostrar.clicked.connect(self.Nifti)
+        self.volver.clicked.connect(self.devolver)
+    def Nifti (self):
+        self.dicom= self.__mi_controlador.cargardicom(self.dicom)
+        self.convertdicom= self.__mi_controlador.convertdicom(self.dicom)
+        self.carpetaNueva = './nifti'
+        for archivo in os.listdir(self.carpetaNueva):
+          if archivo.endswith('.nii.gz'):
+            self.ruta = os.path.join(self.carpetaNueva,archivo)
+            malla = Volume(self.ruta)
+            malla.mode(1).cmap("red") 
+            plt = RayCastPlotter(malla, axes=7)
+            show(malla, zoom=1.2, bg="blue", viewup="z").close() 
+            self.hide()
+            self.__ventanapadre.show()
+    def devolver(self):
+        self.__ventanapadre.show()
+        self.hide()
+    def asignarControlador(self, c):
+        self.__mi_controlador = c
+    
